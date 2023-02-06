053870dfa0c41b913ba259777efe29fc8f61f258
al-zivkovic <aldaman2@outlook.com>
2023-02-05 16:18:03 -0800

    fix

--- initdb.sql
+++ initdb.sql
@@ -2,7 +2,7 @@ CREATE DATABASE data-collection;
 USE data-collection;
 
 CREATE TABLE student_grades (
-    id INTAUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR25) NOT NULL,
     last_name VARCHAR(25) NOT NULL,
     grade INT NOT NULL
