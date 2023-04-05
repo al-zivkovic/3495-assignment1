3eba06b7dc6ae0c2dc49175fd5bc337eab429950
Jacob <jacobescarcha@gmail.com>
2023-04-04 18:30:31 -0700

    Updated analytics

--- analytics/app.py
+++ analytics/app.py
@@ -15,30 +15,38 @@ cursor.execute("SELECT grade FROM data.student_grades")
 data_list = cursor.fetchall()
 data = []
 
-for number in data_list:
-    data.append(number[0])
+def post_grades():
+    for number in data_list:
+        data.append(number[0])
 
-max_value = max(data)
-print(max_value)
+    if len(data) > 0:
+        max_value = max(data)
+        print(max_value)
 
-min_value = min(data)
-print(min_value)
+        min_value = min(data)
+        print(min_value)
 
-avg_value = sum(data) / len(data)
-print(avg_value)
+        avg_value = sum(data) / len(data)
+        print(avg_value)
 
-result = {
-    "max": max_value,
-    "min": min_value,
-    "avg": "{:.2f}".format(avg_value)
-}
+        result = {
+            "max": max_value,
+            "min": min_value,
+            "avg": "{:.2f}".format(avg_value)
+        }
 
-client = pymongo.MongoClient('mongodb://mongodb:27017/')
-db = client['data']
-collection = db['analytics']
+    client = pymongo.MongoClient('mongodb://mongodb:27017/')
+    db = client['data']
+    collection = db['analytics']
 
-collection.delete_many({})
-print("Deleted old stats...")
+    collection.delete_many({})
+    print("Deleted old stats...")
 
-collection.insert_one(result)
-print("Statistics created...")
+    collection.insert_one(result)
+    print("Statistics created...")
+
+while True:
+    try:
+        post_grades()
+    except ZeroDivisionError:
+        print("No values in table yet...")
